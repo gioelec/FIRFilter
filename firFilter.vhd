b4b839a54dc4695776224b4c1ea584b0a32cbe38
gioele95 <gioele95@users.noreply.github.com>
2018-02-20 10:31:35 +0100

    never, never put constants in your code

--- firFilter.vhd
+++ firFilter.vhd
@@ -2,27 +2,26 @@ library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
-constant C0:=0.0135;
-constant C1:=0.0785;
-constant C2:=0.2409;
-constant C3:=0.3344;
-constant C4:=0.2409;
-constant C5:=0.0785;
-constant C6:=0.0135;
+
 entity fir_filter is
-port (
-  clock       : in  std_logic;
-  reset       : in  std_logic;
-  -- coefficient
-  C0   : in  std_logic_vector( 15 downto 0);
-  C1   : in  std_logic_vector( 15 downto 0);
-  C2   : in  std_logic_vector( 15 downto 0);
-  C3   : in  std_logic_vector( 15 downto 0);  
-  C4   : in  std_logic_vector( 15 downto 0);
-  C5   : in  std_logic_vector( 15 downto 0);
-  C6   : in  std_logic_vector( 15 downto 0);
-  -- data input
-  input_data      : in  std_logic_vector( 15 downto 0);
-  -- filtered data 
-  y       : out std_logic_vector( 9 downto 0));
+  generic (
+    coeffs    : real_vector := (0.0135,0.0785,0.2409,0.3344,0.2409,0.0785,0.0135);
+    N         : integer := 16;
+  );
+  port (
+    clock     : in  std_logic;
+    reset     : in  std_logic;
+    -- coefficient
+    C0        : in  std_logic_vector(N-1 downto 0);
+    C1        : in  std_logic_vector(N-1 downto 0);
+    C2        : in  std_logic_vector(N-1 downto 0);
+    C3        : in  std_logic_vector(N-1 downto 0);  
+    C4        : in  std_logic_vector(N-1 downto 0);
+    C5        : in  std_logic_vector(N-1 downto 0);
+    C6        : in  std_logic_vector(N-1 downto 0);
+    -- data input
+    x         : in  std_logic_vector(N-1 downto 0);
+    -- filtered data 
+    y         : out std_logic_vector(9 downto 0));   // still to size
+  );
 end fir_filter;
