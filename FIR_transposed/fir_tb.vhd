6baeb6eec25de38b9720ef3ef1f7f9af178229ce
gioelec <gioelecarignani@gmail.com>
2019-03-11 15:45:15 +0100

    read write I/O from file added

--- FIR_transposed/fir_tb.vhd
+++ FIR_transposed/fir_tb.vhd
@@ -1,6 +1,9 @@
 LIBRARY ieee;
 USE ieee.std_logic_1164.ALL;
 USE ieee.numeric_std.ALL;
+use std.textio.all;  --include package textio.vhd
+use ieee.std_logic_textio.all;
+
 
 ENTITY tb IS
 END tb;
@@ -11,7 +14,8 @@ ARCHITECTURE behavior OF tb IS
   signal X : signed(15 downto 0) := (others => '0');
   signal Y : signed(15 downto 0) := (others => '0');
   constant Clk_period : time := 10 ns;
-
+  file infile : text;
+  file outfile : text;
 BEGIN
 
     -- Instantiate the Unit Under Test (UUT)
@@ -33,19 +37,37 @@ BEGIN
     
    -- Stimulus process
    stim_proc: process
+      variable v_ILINE     : line;
+      variable v_OLINE     : line;
+      variable v_INPUT     : integer;
    begin
+      file_open(infile, "C:\Users\gioel\OneDrive\Desktop\input_vectors.txt",  read_mode);
+      file_open(outfile, "C:\Users\gioel\OneDrive\Desktop\output_results.txt", write_mode);
+   
       Reset <= '1', '0' after 10 ns; 
       wait for Clk_period*2;
-        X <= to_signed(-3,16); wait for clk_period*1;
-        X <= to_signed(1,16); wait for clk_period*1;
-        X <= to_signed(0,16); wait for clk_period*1;
-        X <= to_signed(-2,16); wait for clk_period*1;
-        X <= to_signed(-1,16); wait for clk_period*1;
-        X <= to_signed(4,16); wait for clk_period*1;
-        X <= to_signed(-5,16); wait for clk_period*1;
-        X <= to_signed(6,16); wait for clk_period*1;
-        X <= to_signed(0,16);
-        
+        --X <= to_signed(-3,16); wait for clk_period*1;
+        --X <= to_signed(1,16); wait for clk_period*1;
+        --X <= to_signed(0,16); wait for clk_period*1;
+        --X <= to_signed(-2,16); wait for clk_period*1;
+        --X <= to_signed(-1,16); wait for clk_period*1;
+        --X <= to_signed(4,16); wait for clk_period*1;
+        --X <= to_signed(-5,16); wait for clk_period*1;
+        --X <= to_signed(6,16); wait for clk_period*1;
+        --X <= to_signed(0,16);
+      report "before loop";
+      while not endfile(infile) loop
+        report "loop";   
+        readline(infile, v_ILINE);
+        read(v_ILINE, v_INPUT);   
+        X <= to_signed(v_INPUT,16);
+        wait for clk_period*1;
+        write(v_OLINE,to_integer(signed(Y)));
+        writeline(outfile,v_OLINE);   
+      end loop;
+   
+      file_close(infile);
+      file_close(outfile);
       wait;
    end process;
 
