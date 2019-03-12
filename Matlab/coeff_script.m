9e9420d39ca2589fb7814689992dfd5395710ad6
gioelec <gioelecarignani@gmail.com>
2019-03-12 18:37:28 +0100

    report

--- Matlab/coeff_script.m
+++ Matlab/coeff_script.m
@@ -1,26 +1,4 @@
 b = 16;
-
-%range [-1,1) (C2)
-
 lsb = -(-1) / (2^(b - 1)); 
-
-c = [0.0135,0.0785,0.2409,0.3344,0.2409,0.0785,0.0135]'; %input coefficients
-
-c_i = round(c/lsb);
-
-c_q = c_i * lsb; %quantized coefficients
-
-theta = [0 : 0.1 : 2 * pi];
-
-x = sin(theta);
-
-x_i = round(x / lsb);   %input for the testbench
-
-x_test = x_i';
-
-%o = conv(c_i, x_i);     %output expected
-out = FIR_filter(x_i,c_i);
-
-b_o = ceil(log2(2^(b - 1) * sum(abs(c_i)))); %output bits needed
-
-% a = dec2twos(o,16)
+coeff = [0.0135,0.0785,0.2409,0.3344,0.2409,0.0785,0.0135]'; 
+c_i = round(coeff/lsb);%integer coefficients
--- Report/compile.bat
+++ Report/compile.bat
@@ -1,9 +1,15 @@
 SET main=main
 @echo off
+color 3e
 cls
 echo PDFLATEX --- 1/2
-pdflatex  -aux-directory out/ %main%.tex   
+color 2f
+pdflatex  -aux-directory out/ -output-directory out/ %main%.tex   
 echo PDFLATEX --- 2/2
-pdflatex  -aux-directory out/ -quiet %main%.tex
+color 1d
+pdflatex  -aux-directory out/ -output-directory out/ -quiet %main%.tex
+color
 echo DONE --- OPENING PDF 1/1
-START "" %main%.pdf
+color
+pause
+START "" out/%main%.pdf
