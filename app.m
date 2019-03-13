6e54dc8caadc9357d65f7e7df4ab57f192d94ec2
gioelec <gioelecarignani@gmail.com>
2019-03-13 14:15:19 +0100

    added +1 for 2 complement

--- app.m
+++ app.m
@@ -1,8 +1,6 @@
-c_q = c_i * lsb; %quantized coefficients
-theta = [0 : 0.1 : 2 * pi];
-
-x = sin(theta);
-
+%c_q = c_i * lsb; %quantized coefficients
+theta = [0 : 0.1 : 2  pi];
+x = sin(theta*100);
 x_i = round(x / lsb);   %input for the testbench
 
 x_test = x_i';
@@ -10,6 +8,6 @@ x_test = x_i';
 %o = conv(c_i, x_i);     %output expected
 out = FIR_filter(x_i,c_i);
 
-b_o = ceil(log2(2^(b - 1) * sum(abs(c_i)))); %output bits needed
+b_o = ceil(log2(2^(b - 1) * sum(abs(c_i))))+1; %output bits needed
 
 % a = dec2twos(o,16)
