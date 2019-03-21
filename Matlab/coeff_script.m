392041ee6647bbb9b1bb2df4b3d17a9e373dbc32
gioele95 <gioelecarignani@gmail.com>
2019-03-21 13:43:27 +0100

    reordering

--- Matlab/coeff_script.m
+++ Matlab/coeff_script.m
@@ -1,4 +1,4 @@
 b = 16;
-lsb = -(-1) / (2^(b - 1)); 
+lsb = -(-1) / (2^(b - 1)); %lsb for converting double to int
 coeff = [0.0135,0.0785,0.2409,0.3344,0.2409,0.0785,0.0135]'; 
 c_i = round(coeff/lsb);%integer coefficients
